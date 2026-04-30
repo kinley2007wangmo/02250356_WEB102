68f0ded4e13e403eea7a85a59b0f175234f36a3e
Kinley Wangmo <kineywamo@Kinleys-MacBook-Pro.local>
2026-04-30 09:18:03 +0600

    Folder name changed and update exercise work

--- Exercise/Exercise_1/db-connection/server.js
+++ Exercise/Exercise_1/db-connection/server.js
@@ -1,11 +1,76 @@
-console.log("THIS IS MY SERVER FILE");
 const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('SERVER WORKING');
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// PostgreSQL connection
+const pool = new Pool({
+  user: 'kineywamo',
+  host: 'localhost',
+  database: 'student_records',
+  password: '', // add password if you set one
+  port: 5432,
 });
 
-app.listen(3000, () => {
-  console.log('Server running on 3000');
+pool.connect()
+  .then(client => {
+    console.log('Connected to PostgreSQL database!');
+    client.release();
+    })
+
+  .catch(err => {
+    console.error('PostgreSQL connection error:', err);
+  });
+
+app.get('/',  (req, res) => {
+    res.send('Student Records API is running (PostgreSQL)');
+});
+
+// Get all students
+app.get('/api/students', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM students');
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error fetching students:', err);
+    res.status(500).json({error: 'Failed to fetch students' });
+  }
+});
+
+//  POST API:Add a new student
+app.post('/api/students', async (req, res) => {
+  const { name, email, course, enrollment_date } = req.body;
+
+  //  Validation
+  if (!name || !email ||!course ||!enrollment_date) {
+    return res.status(400).json({ error: 'All field are required' });
+  }
+
+  try {
+    const result = await pool.query(
+      'INSERT INTO students (name, email, course, enrollment_date) VALUES ($1, $2, $3, $4) RETURNING *',
+      [name, email, course, enrollment_date]
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error('Error adding student:', err);
+  
+  if (err.code === '23505') { // Unique violation
+      res.status(400).json({ error: 'Email already exists' });
+    } 
+
+    res.status(500).send({error:'Failed to add student'});
+  }
+});
+
+// Start server
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
 });
